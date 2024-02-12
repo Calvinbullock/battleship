5f83dc1e0f7ad0394fcf9dcc7882e252116f5003
Calvinbullock <calvinnbullock@gmail.com>
2024-02-12 11:39:31 -0700

    basic hello world and todo comment

---
+++ main.go
@@ -0,0 +1,23 @@
+// Calvin Bullock
+// BattleShip main filr
+
+
+/* TODO
+Minimum project
+- two player
+- console boards (yours and radar)
+- player switching
+Extra
+- two player with computer player.
+- if a hit is found by computer.
+- check in points around the hit until sunk.
+- save state
+*/
+
+package main
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
