3f5f480ed13b8a643c6598f929d097daa1aa423a
Calvinbullock <calvinnbullock@gmail.com>
2024-02-12 11:51:17 -0700

    todo outline

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 // Calvin Bullock
-// BattleShip main filr
+// BattleShip main file
 
 
 /* TODO
@@ -12,6 +12,20 @@ Extra
 - if a hit is found by computer.
 - check in points around the hit until sunk.
 - save state
+
+------ TODO
+Finish planing doc
+Install go-lang build / run hello world. Set up git repro.
+Set up boards, 2 for each user (player board, radar). Allow player modification and print them all out.
+Get clean print out of boards, allow switching between players boards.
+UI – start menu
+Ship placement and storage, sinking, display hits, missus, etc.
+Clean implement all rules and game over checks.
+Start computer player if time, if not work on fixing bugs and finishing minimum requirements.
+Find good algorithms used by battleship players for the computer.
+More computer player work and clean up, QA
+Fill out document, make video final clean up and formfitting.
+
 */
 
 package main
